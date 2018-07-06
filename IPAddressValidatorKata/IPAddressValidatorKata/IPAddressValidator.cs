8f6e32693b8bc67dd524ee6e2fc87405336c76c9
sinothilem <sinothilem@D987321>
2018-07-06 14:08:44 +0200

    done

---
+++ IPAddressValidatorKata/IPAddressValidatorKata/IPAddressValidator.cs
@@ -0,0 +1,50 @@
+﻿namespace IPAddressValidatorKata
+{
+    public class IPAddressValidator
+    {
+        public bool ValidateIpv4Address(string ipAddress)
+        {
+            if (IpAddressIsEmpty(ipAddress))
+            {
+                return false;
+            }
+            var splitedIpAddress = GetSplitedIpAddress(ipAddress);
+            var ipAddressLength = GetIpAddressLength(splitedIpAddress);
+            if (IpAddressNotGroupedInto4OneByteBlocks(ipAddressLength))
+            {
+                return false;
+            }
+
+            if (IpAddressEndsWith255OrZero(splitedIpAddress))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool IpAddressEndsWith255OrZero(string[] splitedIpAddress)
+        {
+            return splitedIpAddress[3] == "255" || splitedIpAddress[3] == "0";
+        }
+
+        private static bool IpAddressNotGroupedInto4OneByteBlocks(int ipAddressLength)
+        {
+            return ipAddressLength != 4;
+        }
+
+        private static int GetIpAddressLength(string[] splitedIpAddress)
+        {
+            return splitedIpAddress.Length;
+        }
+
+        private static string[] GetSplitedIpAddress(string ipAddress)
+        {
+            return ipAddress.Split('.');
+        }
+
+        private static bool IpAddressIsEmpty(string ipAddress)
+        {
+            return string.IsNullOrWhiteSpace(ipAddress);
+        }
+    }
+}
---
+++ IPAddressValidatorKata/IPAddressValidatorKataTests/IPAddressValidatorTests.cs
@@ -0,0 +1,74 @@
+﻿using IPAddressValidatorKata;
+using NUnit.Framework;
+
+
+namespace IPAddressValidatorKataTests
+{
+    [TestFixture]
+    public class IPAddressValidatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void ValidateIpv4Address_GivenEmptyOrNullIPAddress_ShouldReturnFalse(string ipAddress)
+        {
+            //Arrange
+            var sut = new IPAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1")]
+        [TestCase("5.9")]
+        [TestCase("3.6.3")]
+        public void ValidateIpv4Address_GivenIpAddressLessThan4Blocks_ShouldReturnFalse(string ipAddress)
+        {
+            //Arrange
+            var sut = new IPAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1.1.1.1")]
+        [TestCase("5.9.2.2")]
+        [TestCase("3.6.3.4")]
+        public void ValidateIpv4Address_GivenIpAddressWith4Blocks_ShouldReturnTrue(string ipAddress)
+        {
+            //Arrange
+            var sut = new IPAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            var expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("255.255.255.255")]
+        [TestCase("255.255.255.0")]
+        [TestCase("1.1.1.0")]
+        public void ValidateIpv4Address_GivenIpAddressThatEndsWith2550rZero_ShouldReturnFalse(string ipAddress)
+        {
+            //Arrange
+            var sut = new IPAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpv4Address(ipAddress);
+
+            //Assert
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
